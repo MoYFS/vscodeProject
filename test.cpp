b23f22d18dd70f197d48bd8b7ae200c461d690e1
莫语浮生 <zhangjiarui2018@outlook.com>
2023-04-27 10:43:00 +0800

    更新

--- test.cpp
+++ test.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main() {
-    cout << "hello world" << endl;
+    printf("%d\n",'6');
+    printf("%c\n",54);
+    int c=54;
+    char m=char(c);
+
+    cout << m << endl;
     return 0;
 }
