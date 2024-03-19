cb7c9e2501247e8d5f676031cfd310b691fa8941
moyufusheng <2331010886@qq.com>
2024-03-19 12:15:16 +0800

    webpack学习

--- front-end development/change-row-color/src/index.js
+++ front-end development/change-row-color/src/index.js
@@ -1,6 +1,10 @@
 import $ from 'jquery'
 //导入样式（在webpack中，一切皆模块，都可以通过ES6导入语法进行导入使用）
 import './css/index.css'
+import './css/index.less'
+import logo from './images/logo.jpg'
+
+$('.box').attr('src', logo)
 
 $(function () {
     $('li:odd').css('background-color','red')
--- front-end development/change-row-color/webpack.config.js
+++ front-end development/change-row-color/webpack.config.js
@@ -21,7 +21,9 @@ module.exports={
     },
     module: {
         rules: [
-            { test: /\.css$/, use:['style-loader', 'css-loader'] }
+            { test: /\.css$/, use:['style-loader', 'css-loader'] },
+            { test: /\.less$/, use:['style-loader', 'css-loader','less-loader'] },
+            { test: /\.jpg|png|gif$/, use:'url-loader?limit=22229'}
         ]
     }
 
